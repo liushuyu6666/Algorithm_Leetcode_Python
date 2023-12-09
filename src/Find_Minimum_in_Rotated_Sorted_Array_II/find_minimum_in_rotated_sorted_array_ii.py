78381fb10fe57da051f5546a0435d151b9787790
shuyu liu <liushuyu6666@gmail.com>
2023-12-09 10:03:27 -0500

    add code comment in Find_Minimum_in_Rotated_Sorted_Array_II

--- src/Find_Minimum_in_Rotated_Sorted_Array_II/find_minimum_in_rotated_sorted_array_ii.py
+++ src/Find_Minimum_in_Rotated_Sorted_Array_II/find_minimum_in_rotated_sorted_array_ii.py
@@ -4,15 +4,17 @@ from typing import List
 class Solution:
     def findMin(self, nums: List[int]) -> int:
         def dfs(left: int, right: int) -> int:
-            if left == right:
+            if left == right:  # end condition: left == right
                 return nums[left]
 
             mid = (left + right) // 2
 
             if nums[mid] < nums[right]:
+                # Update Right Pointer: Ensure strict mode (right = mid) as nums[mid] is possible the minimum value.
                 return dfs(left, mid)
             elif nums[mid] > nums[right]:
-                return dfs(mid + 1, right)
+                # Update Left Pointer: Employ strict mode (left = mid + 1) as nums[mid] is not the minimum value.
+                return dfs(mid + 1, right) # update the left pointer: left = mid + 1, strict
             else:
                 return min(dfs(left, mid), dfs(mid + 1, right))
 
